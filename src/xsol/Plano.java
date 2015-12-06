90cfc83d42be1dec6f7992708088f9488da1dcd5
CrazyKatana <keimzely3@hotmail.com>
2015-12-06 07:40:53 -0600

    Update Plano.java

--- src/xsol/Plano.java
+++ src/xsol/Plano.java
@@ -15,9 +15,42 @@ import javax.swing.JPanel;
  * @author AlbertoReyes
  */
 public class Plano extends JPanel{
-    
-    private int ancho;
-    private int alto;
+    int ancho = 0;
+    int alto = 0;
+    double X1, Y1, Ent;
+
+    public double getX1() {
+        return X1;
+    }
+
+    public void setX1(double X) {
+        this.X1 = X;
+    }
+
+    public double getY1() {
+        return Y1;
+    }
+
+    public void setY1(double Y) {
+        this.Y1 = Y;
+    }
+
+    public double getEnt() {
+        return Ent;
+    }
+
+    public void setEnt(double Ent) {
+        this.Ent = Ent;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    int tipo;
     /*public Plano(int ancho, int alto){
         
         setBackground(Color.WHITE);
@@ -43,12 +76,11 @@ public class Plano extends JPanel{
         
     }*/
     public Plano(int ancho, int alto){
-        
+        this.ancho =ancho;
+        this.alto = alto;
         init();
         setSize(ancho, alto);
         setLocation(0,0);
-        this.ancho  = ancho;
-        this.alto = alto;
         
     }
     public void init(){
@@ -61,14 +93,55 @@ public class Plano extends JPanel{
         
         super.paintComponent(g);
         g.setColor(new Color(177,228,255));
-        double ss = .025;
+        double los40divisiones = .000;
         for(int i= 0;i<40;i++) {
             
-            g.drawLine(0, (int)Math.floor(this.getHeight()*ss) , ancho, (int)Math.floor(this.getHeight()*ss));
-            g.drawLine((int)Math.floor(this.getWidth()*ss),0 , (int)Math.floor(this.getWidth()*ss), alto);
-            ss += .025;
+            g.drawLine(0, (int)Math.floor(this.getHeight()*los40divisiones) , ancho, (int)Math.floor(this.getHeight()*los40divisiones));
+            g.drawLine((int)Math.floor(this.getWidth()*los40divisiones),0 , (int)Math.floor(this.getWidth()*los40divisiones), alto);
+            los40divisiones += .025;
             
         }
+        //PINTAR GRAFICA INGRESADA
+        if(tipo!=0) {
+            
+            //Recta
+            //ME PARTI EL ORTO CON ESTA :v
+            if(this.tipo==1) {
+                g.setColor(new Color(115, 255, 147));
+                g.setColor(Color.BLACK);
+                int x1=80;
+                int x2=79;
+                double las160divisiones = .00000;
+                los40divisiones = .000;
+                for(int z = 0; z<160;z++) {
+                        double Y5 = ((-((this.X1)*x1)) -(this.Ent))/this.Y1;
+                        double Y2 = ((-((this.X1)*x2)) -(this.Ent))/this.Y1;
+                        int y1graf = (int)Math.floor((this.getHeight()/2)-(Y5*(getHeight()*0.025)));
+                        int y2graf = (int)Math.floor((this.getHeight()/2)-(Y2*(getHeight()*0.025)));
+                        int x1graf = (int)Math.floor((this.getWidth()/2)+(x1*(getWidth()*0.025)));
+                        los40divisiones += .025;
+                        int x2graf = (int)Math.floor((this.getWidth()/2)+(x2*(getWidth()*0.025)));
+                        x1=x2;
+                        x2--;
+                        g.drawLine(x1graf, y1graf, x2graf, y2graf);
+                
+                    }
+                }
+            //AQUI HAZ LAS OTRAS PINSHES TIPOS DE GRAFICAS
+                
+                
+            }
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
         g.setColor(Color.red);
         g.drawLine(0, this.getHeight() / 2, this.getWidth(), this.getHeight() / 2);
         g.drawLine(this.getWidth() / 2, 0, this.getWidth()/2, this.getHeight());
@@ -81,3 +154,4 @@ public class Plano extends JPanel{
         
     }
 }
+
