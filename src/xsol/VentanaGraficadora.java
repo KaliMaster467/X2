181461e9bfbcb733cef5da4dd99779e640ee7844
CrazyKatana <keimzely3@hotmail.com>
2015-12-06 12:31:28 -0600

    Update VentanaGraficadora.java

--- src/xsol/VentanaGraficadora.java
+++ src/xsol/VentanaGraficadora.java
@@ -49,13 +49,13 @@ public class VentanaGraficadora extends Ventana{
     }
     
     public class TablaRes extends JPanel implements ItemListener, ActionListener{
-    JLabel X, Y, tabla, xValor, yValor, enteros;
+    JLabel X, Y, tabla, xValor, yValor, enteros, x2Valor, y2Valor;
     JComboBox tipoTabla;
     private Font fuente;
     private ImageIcon imagelap;
-    JTextField valorX, valorY, valorEnt;
+    JTextField valorX, valorY, valorEnt, valorX2, valorY2;
     JButton Grafica;
-    
+    private int tipograf;
     
     
     
@@ -69,7 +69,7 @@ public class VentanaGraficadora extends Ventana{
         setSize(anchos, altos);
         //PUSE EL JBUTTON PARA GRAFICAR
         Grafica = new JButton("Grafica!");
-        Grafica.setBounds(120, 110, 110, 30);
+        Grafica.setBounds(140, 150, 110, 30);
         Grafica.setVisible(true);
         Grafica.addActionListener(this);
         
@@ -78,23 +78,43 @@ public class VentanaGraficadora extends Ventana{
         valorX = new JTextField();
         valorX.setBounds(30, 70, 80, 30);
         valorX.setText("");
-        valorX.setVisible(true);
+        valorX.setVisible(false);
         valorX.setEnabled(false);
         valorX.addActionListener(this);
         
         add(valorX);
         
+        valorX2 = new JTextField();
+        valorX2.setBounds(30, 100, 80, 30);
+        valorX2.setText("");
+        valorX2.setVisible(false);
+        valorX2.setEnabled(false);
+        valorX2.addActionListener(this);
+        
+        add(valorX2);
+        
         valorY = new JTextField();
-        valorY.setBounds(135, 70, 80, 30);
+        valorY.setBounds(140, 70, 80, 30);
+        valorY.setText("");
         valorY.setEnabled(false);
+        valorY.setVisible(false);
         valorY.addActionListener(this);
         
         add(valorY);
         
+        valorY2 = new JTextField();
+        valorY2.setBounds(140, 100, 80, 30);
+        valorY2.setText("");
+        valorY2.setEnabled(false);
+        valorY2.setVisible(false);
+        valorY2.addActionListener(this);
+        
+        add(valorY2);
+        
         valorEnt = new JTextField();
         valorEnt.setBounds(280, 70, 80, 30);
         valorEnt.setText("");
-        valorEnt.setVisible(true);
+        valorEnt.setVisible(false);
         valorEnt.setEnabled(false);
         valorEnt.addActionListener(this);
         
@@ -111,7 +131,7 @@ public class VentanaGraficadora extends Ventana{
         //LOS JLABEL DE LOS VALORES
         enteros = new JLabel();
         enteros.setText("Entero: ");
-        enteros.setVisible(true);
+        enteros.setVisible(false);
         enteros.setLayout(null);
         enteros.setFont(fuente);
         enteros.setLocation(220, 70);
@@ -120,9 +140,29 @@ public class VentanaGraficadora extends Ventana{
         
         add(enteros);
         
+        x2Valor = new JLabel();
+        x2Valor.setText("Y²: ");
+        x2Valor.setVisible(false);
+        x2Valor.setLayout(null);
+        x2Valor.setFont(fuente);
+        x2Valor.setBounds(10, 100, 500, 30);
+        x2Valor.setForeground(Color.WHITE);
+        
+        add(x2Valor);
+        
+        y2Valor = new JLabel();
+        y2Valor.setText("Y²: ");
+        y2Valor.setVisible(false);
+        y2Valor.setLayout(null);
+        y2Valor.setFont(fuente);
+        y2Valor.setBounds(115, 100, 500, 30);
+        y2Valor.setForeground(Color.WHITE);
+        
+        add(y2Valor);
+        
         yValor = new JLabel();
         yValor.setText("y: ");
-        yValor.setVisible(true);
+        yValor.setVisible(false);
         yValor.setLayout(null);
         yValor.setFont(fuente);
         yValor.setLocation(115, 70);
@@ -133,7 +173,7 @@ public class VentanaGraficadora extends Ventana{
         
         xValor = new JLabel();
         xValor.setText("x: ");
-        xValor.setVisible(true);
+        xValor.setVisible(false);
         xValor.setLayout(null);
         xValor.setFont(fuente);
         xValor.setLocation(10, 70);
@@ -149,7 +189,7 @@ public class VentanaGraficadora extends Ventana{
         tabla.setVisible(true);
         tabla.setLayout(null);
         tabla.setFont(fuente);
-        tabla.setLocation(50, 140);
+        tabla.setLocation(50, 180);
         tabla.setSize(500, 30);
         tabla.setForeground(Color.WHITE);
         
@@ -160,7 +200,7 @@ public class VentanaGraficadora extends Ventana{
         X.setVisible(true);
         X.setLayout(null);
         X.setFont(fuente);
-        X.setLocation(50, 170);
+        X.setLocation(50, 210);
         X.setSize(200, 30);
         X.setForeground(Color.WHITE);
         
@@ -170,7 +210,7 @@ public class VentanaGraficadora extends Ventana{
         Y.setVisible(true);
         Y.setLayout(null);
         Y.setFont(fuente);
-        Y.setLocation(200, 170);
+        Y.setLocation(200, 210);
         Y.setSize(200, 30);
         Y.setForeground(Color.WHITE);
         
@@ -183,26 +223,99 @@ public class VentanaGraficadora extends Ventana{
     public void itemStateChanged(ItemEvent e) {
         if (e.getStateChange() == ItemEvent.SELECTED) {
             if (e.getSource() instanceof JComboBox) {
-                valorX.setEnabled(true);
-                valorY.setEnabled(true);
-                valorEnt.setEnabled(true);
+                
                 
                 int cual = tipoTabla.getSelectedIndex();
                 
                 //AQUI SE LE PASA EL TIPO DE GRAFICA A EL PLANO
+                //TEXT FIELD VALOR AL FINAL
                 if (cual != 0) {
                     if (cual == 1) {
+                        JOptionPane.showMessageDialog(null, "Llena todos los espacios");
+                        valorX.setEnabled(true);
+                        valorY.setEnabled(true);
+                        valorEnt.setEnabled(true);
                         plano.setTipo(cual);
+                        tipograf = 1;
+                        valorX.setText("");
+                        valorX.setVisible(true);
+                        valorY.setText("");
+                        valorY.setVisible(true);
+                        valorEnt.setText("");
+                        valorEnt.setVisible(true);
+                        xValor.setVisible(true);
+                        xValor.setText("x: ");
+                        yValor.setVisible(true);
+                        yValor.setText("y: ");
+                        enteros.setVisible(true);
+                        enteros.setText("Entero: ");
+                        x2Valor.setVisible(false);
+                        x2Valor.setText("");
+                        
+                        valorX2.setVisible(false);
+                        valorX2.setText("");
+                        
+                        y2Valor.setVisible(false);
+                        y2Valor.setText("");
+                        valorY2.setVisible(false);
+                        valorY2.setText("");
+                        
+                        
                         
                     } else {
                         if (cual == 2) {
-                            plano.setTipo(cual);
+                         char verticalXOY = 'z';
+                         while(verticalXOY!='x'&&verticalXOY!='X'&&verticalXOY!='y'&&verticalXOY!='Y') {
+                          verticalXOY = (JOptionPane.showInputDialog(null, "La elipse es paralela en X o en Y? (Teclea X o Y)").charAt(0));
+                          if(verticalXOY=='x'||verticalXOY=='y'||verticalXOY=='X'||verticalXOY=='Y') { 
+                            
+                            tipograf = 2;
+                            
+                            valorY.setEnabled(true);
+                            valorEnt.setEnabled(true);
+                            valorX.setVisible(true);
+                            valorY.setText("");
+                            valorY.setVisible(true);
+                            valorEnt.setText("");
+                            valorEnt.setVisible(true);
+                            valorX2.setText("");
+                            valorX2.setVisible(true);
+                            xValor.setVisible(true);
+                            xValor.setText("X²: ");
+                            yValor.setVisible(true);
+                            yValor.setText("X: ");
+                            enteros.setVisible(true);
+                            enteros.setText("Entero: ");
+                            x2Valor.setVisible(true);
+                            x2Valor.setText("Y²");
+                            y2Valor.setVisible(true);
+                            y2Valor.setText("Y: ");
+                            valorY2.setVisible(true);
+                            valorY2.setText("");
+                            valorX.setText("");
+                            valorY2.setEnabled(true);
+                            if(verticalXOY=='x'||verticalXOY=='X') {
+                                plano.setTipo(21);
+                                valorX2.setEnabled(true);
+                                valorX.setEnabled(false);
+                            } else {
+                                plano.setTipo(22);
+                                valorX.setEnabled(true);
+                                valorX2.setEnabled(false);
+                            }
+                            
+                            
+                          }
+                         }
+                         verticalXOY ='z';
                         } else {
                             if (cual == 3) {
                                 plano.setTipo(cual);
+                                tipograf = 3;
                             } else {
                                 if (cual == 4) {
                                     plano.setTipo(cual);
+                                    tipograf = 4;
                                 }
                             }
                         }
@@ -228,9 +341,31 @@ public class VentanaGraficadora extends Ventana{
         public void actionPerformed(ActionEvent e) {
             if(e.getActionCommand().equals("Grafica!")) {
                 //AQUI SE PASA EL VALOR DE X Y EL ENTERO Y SE HACE EL REPAINT
-                plano.setX1(Double.parseDouble(valorX.getText()));
-                plano.setY1(Double.parseDouble(valorY.getText()));
-                plano.setEnt(Double.parseDouble(valorEnt.getText()));
+                
+                if(tipograf == 1) {
+                    plano.setX1(Double.parseDouble(valorX.getText()));
+                    plano.setY1(Double.parseDouble(valorY.getText()));
+                    plano.setEnt(Double.parseDouble(valorEnt.getText()));
+                    String formula = String.format("| Y= (-%sX-%s)/%s  |", valorX.getText(), valorEnt.getText(), valorY.getText());
+                    Y.setText(formula);
+                }else if(plano.getTipo()>20){
+                    if(plano.getTipo()==21) {
+                        plano.setY1(Double.parseDouble(valorY.getText()));
+                        plano.setY2(Double.parseDouble(valorY2.getText()));
+                        //valorX2 equivale a Y al cuadrado y valor Y2 equivale a Y normal X1 equivale a X2 y X2 a X
+                        plano.setX2(Double.parseDouble(valorX2.getText()));
+                        plano.setEnt(Double.parseDouble(valorEnt.getText()));
+                        String formula = String.format("| Y= (-%sX-%s)/%s  |", valorX.getText(), valorEnt.getText(), valorY.getText());
+                    }
+                    else {
+                        plano.setX1(Double.parseDouble(valorX.getText()));
+                        plano.setY1(Double.parseDouble(valorY.getText()));
+                        plano.setEnt(Double.parseDouble(valorEnt.getText()));   
+                        plano.setY2(Double.parseDouble(valorY2.getText()));
+                        String formula = String.format("| √Y= (-%sX-%s)/%s  |", valorX.getText(), valorEnt.getText(), valorY.getText());
+                    }
+                }
+                
                 
                 plano.repaint();
                 
@@ -240,3 +375,4 @@ public class VentanaGraficadora extends Ventana{
     }
     
 }
+
