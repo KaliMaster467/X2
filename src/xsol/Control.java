e37c5b4befb002a7cb30f71c51dfd9446e5547a7
CrazyKatana <keimzely3@hotmail.com>
2015-12-06 12:28:06 -0600

    Update Control.java

--- src/xsol/Control.java
+++ src/xsol/Control.java
@@ -8,6 +8,7 @@ package xsol;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -21,12 +22,15 @@ public class Control implements ActionListener{
     protected VentanaGraficadora graf;
     protected Registro reg;
     protected ListaUsuario users;
+    private Archivo archivo = new Archivo();
     private Manejador man;
+    public static ArrayList<Usuario> Usuarios = new ArrayList<>();
     
-    public Control(){
-        
+    public void Controls(){
+        Usuarios = archivo.leer();
         try{
             ini = new VentanaInicion();
+            ini.VentanaInicions();
         }catch(IOException e){
             
             JOptionPane.showMessageDialog(null, "Error");
@@ -51,11 +55,31 @@ public class Control implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == ini.accion){
-            
-           reg = new Registro(); 
-            
+        if(e.getActionCommand().equals("Iniciar")) {
+            int encontrado = 0;
+            for(int i=0;i<Usuarios.size();i++) {
+                if(Usuarios.get(i).getNombre().equals(ini.getUserReturn())) {
+                    
+                    if(Usuarios.get(i).getClave().equals(ini.getPassReturn())) {
+                        JOptionPane.showMessageDialog(null, "Bienvenido "+Usuarios.get(i).getNombre());
+                        VentanaGraficadora graficadora = new VentanaGraficadora();
+                        ini.dispose();
+                        encontrado = 1;
+                        break;
+                    }
+                }
+                
+            }
+            if(encontrado ==0)
+                    JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+        }
+        else if(e.getActionCommand().equals("Registro")) {
+            Registro reg = new Registro();
         }
     }
+
+        
+        }
     
-}
+    
+
