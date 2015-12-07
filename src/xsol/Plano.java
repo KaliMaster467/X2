38a9bc0e7e9e071a8aac1f90dece001e3e5751ee
CrazyKatana <keimzely3@hotmail.com>
2015-12-07 01:18:06 -0600

    Update Plano.java

--- src/xsol/Plano.java
+++ src/xsol/Plano.java
@@ -14,22 +14,23 @@ import javax.swing.JPanel;
  *
  * @author AlbertoReyes
  */
-public class Plano extends JPanel{
+public class Plano extends JPanel {
+
     int ancho = 0;
     int alto = 0;
     double X1, Y1, Ent;
     double X2, Y2;
     double YR1, YR2, YP1, YP2, XP1, XP2;//
     double YE1, YE2, XE1, XE2;
-    int tipoElipse=0;
-    double BX, CX;
+    int tipoElipse = 0;
+    double AX, CX;
 
-    public double getBX() {
-        return BX;
+    public double getAX() {
+        return AX;
     }
 
-    public void setBX(double BX) {
-        this.BX = BX;
+    public void setAX(double AX) {
+        this.AX = AX;
     }
 
     public double getCX() {
@@ -40,7 +41,6 @@ public class Plano extends JPanel{
         this.CX = CX;
     }
 
-
     public int getTipoElipse() {
         return tipoElipse;
     }
@@ -48,7 +48,6 @@ public class Plano extends JPanel{
     public void setTipoElipse(int tipoElipse) {
         this.tipoElipse = tipoElipse;
     }
-
 
     public double getX1() {
         return X1;
@@ -98,6 +97,7 @@ public class Plano extends JPanel{
     public void setY2(double Y2) {
         this.Y2 = Y2;
     }
+
     /*public Plano(int ancho, int alto){
         
         setBackground(Color.WHITE);
@@ -122,154 +122,171 @@ public class Plano extends JPanel{
         g.fillRect(this.getWidth() / 2, 0, 3, this.getHeight());
         
     }*/
-    public Plano(int ancho, int alto){
-        this.ancho =ancho;
+    public Plano(int ancho, int alto) {
+        this.ancho = ancho;
         this.alto = alto;
         init();
         setSize(ancho, alto);
-        setLocation(0,0);
-        
+        setLocation(0, 0);
+
     }
-    public void init(){
-        
+
+    public void init() {
+
         this.setBorder(BorderFactory.createLineBorder(Color.black));
-        
+
     }
+
     @Override
-    public void paintComponent(Graphics g){
-        
+    public void paintComponent(Graphics g) {
+
         super.paintComponent(g);
-        g.setColor(new Color(177,228,255));
+        g.setColor(new Color(177, 228, 255));
         double los40divisiones = .000;
-        for(int i= 0;i<40;i++) {
-            
-            g.drawLine(0, (int)Math.floor(this.getHeight()*los40divisiones) , ancho, (int)Math.floor(this.getHeight()*los40divisiones));
-            g.drawLine((int)Math.floor(this.getWidth()*los40divisiones),0 , (int)Math.floor(this.getWidth()*los40divisiones), alto);
+        for (int i = 0; i < 40; i++) {
+
+            g.drawLine(0, (int) Math.floor(this.getHeight() * los40divisiones), ancho, (int) Math.floor(this.getHeight() * los40divisiones));
+            g.drawLine((int) Math.floor(this.getWidth() * los40divisiones), 0, (int) Math.floor(this.getWidth() * los40divisiones), alto);
             los40divisiones += .025;
-            
+
         }
         //PINTAR GRAFICA INGRESADA
-        if(tipo!=0) {
-                
+        if (tipo != 0) {
+
             //Recta
             //ME PARTI EL ORTO CON ESTA :v
-            if(this.tipo==1) {
+            if (this.tipo == 1) {
                 g.setColor(new Color(0, 24, 125));
-                double x1=80;//Tambien sirven como ys
-                double x2=79;
-                for(int z = 0; z<160;z++) {
-                        YR1 = ((-((this.X1)*x1)) -(this.Ent))/this.Y1;
-                        YR2 = ((-((this.X1)*x2)) -(this.Ent))/this.Y1;
-                        int y1graf = (int)Math.floor((this.getHeight()/2)-(YR1*(getHeight()*0.025)));
-                        int y2graf = (int)Math.floor((this.getHeight()/2)-(YR2*(getHeight()*0.025)));
-                        int x1graf = (int)Math.floor((this.getWidth()/2)+(x1*(getWidth()*0.025)));
+                double x1 = 80;//Tambien sirven como ys
+                double x2 = 79;
+                for (int z = 0; z < 160; z++) {
+                    YR1 = ((-((this.X1) * x1)) - (this.Ent)) / this.Y1;
+                    YR2 = ((-((this.X1) * x2)) - (this.Ent)) / this.Y1;
+                    int y1graf = (int) Math.floor((this.getHeight() / 2) - (YR1 * (getHeight() * 0.025)));
+                    int y2graf = (int) Math.floor((this.getHeight() / 2) - (YR2 * (getHeight() * 0.025)));
+                    int x1graf = (int) Math.floor((this.getWidth() / 2) + (x1 * (getWidth() * 0.025)));
 
-                        int x2graf = (int)Math.floor((this.getWidth()/2)+(x2*(getWidth()*0.025)));
-                        x1=x2;
-                        x2--;
-                        g.drawLine(x1graf, y1graf, x2graf, y2graf);
-                
-                    }
-                
+                    int x2graf = (int) Math.floor((this.getWidth() / 2) + (x2 * (getWidth() * 0.025)));
+                    x1 = x2;
+                    x2--;
+                    g.drawLine(x1graf, y1graf, x2graf, y2graf);
+
                 }
 
-            //PARABOLA
-            else if(this.tipo>20) {
-                double x1=80;//Tambien sirven como ys
-                double x2=79.875;
-                g.setColor(new Color(186,24,74));
-                for(int z = 0; z<2560;z++) {
-                    if(this.tipo==21) {
-                        double Elevada =X2*x1;
-                        double Elevada1 =X2*x2;
-                        
-                        XP1 = (Ent+(Y2*x1)+(Elevada * Elevada))/-Y1;
-                        XP2 = (Ent+(Y2*x2)+(Elevada1 * Elevada1))/-Y1;
-                        int x1graf = (int)Math.floor((this.getWidth()/2)+(XP1*(getWidth()*0.025)));
-                        int x2graf = (int)Math.floor((this.getWidth()/2)+(XP2*(getWidth()*0.025)));
-                        int y1graf = (int)Math.floor((this.getHeight()/2)-(x1*(getHeight()*0.025)));
-                        int y2graf = (int)Math.floor((this.getHeight()/2)-(x2*(getHeight()*0.025)));
-                        x1=x2;
-                        x2=x2-.125;
+            } //PARABOLA
+            else if (this.tipo > 20) {
+                double x1 = 80;//Tambien sirven como ys
+                double x2 = 79.875;
+                g.setColor(new Color(186, 24, 74));
+                for (int z = 0; z < 2560; z++) {
+                    if (this.tipo == 21) {
+                        double Elevada = X2 * x1;
+                        double Elevada1 = X2 * x2;
+
+                        XP1 = (Ent + (Y2 * x1) + (Elevada * Elevada)) / -Y1;
+                        XP2 = (Ent + (Y2 * x2) + (Elevada1 * Elevada1)) / -Y1;
+                        int x1graf = (int) Math.floor((this.getWidth() / 2) + (XP1 * (getWidth() * 0.025)));
+                        int x2graf = (int) Math.floor((this.getWidth() / 2) + (XP2 * (getWidth() * 0.025)));
+                        int y1graf = (int) Math.floor((this.getHeight() / 2) - (x1 * (getHeight() * 0.025)));
+                        int y2graf = (int) Math.floor((this.getHeight() / 2) - (x2 * (getHeight() * 0.025)));
+                        x1 = x2;
+                        x2 = x2 - .125;
                         g.drawLine(x1graf, y1graf, x2graf, y2graf);
-                    }else {
-                        double Elevada =X1*x1;
-                        double Elevada1 =X1*x2;
-                        
-                        YP1 = (Ent+(Y1*x1)+(Elevada * Elevada))/-Y2;
-                        YP2 = (Ent+(Y1*x2)+(Elevada1 * Elevada1))/-Y2;
-                        int x1graf = (int)Math.floor((this.getWidth()/2)+(x1*(getWidth()*0.025)));
-                        int x2graf = (int)Math.floor((this.getWidth()/2)+(x2*(getWidth()*0.025)));
-                        int y1graf = (int)Math.floor((this.getHeight()/2)-(YP1*(getHeight()*0.025)));
-                        int y2graf = (int)Math.floor((this.getHeight()/2)-(YP2*(getHeight()*0.025)));
-                        x1=x2;
-                        x2=x2-.125;
+                    } else {
+                        double Elevada = X1 * x1;
+                        double Elevada1 = X1 * x2;
+
+                        YP1 = (Ent + (Y1 * x1) + (Elevada * Elevada)) / -Y2;
+                        YP2 = (Ent + (Y1 * x2) + (Elevada1 * Elevada1)) / -Y2;
+                        int x1graf = (int) Math.floor((this.getWidth() / 2) + (x1 * (getWidth() * 0.025)));
+                        int x2graf = (int) Math.floor((this.getWidth() / 2) + (x2 * (getWidth() * 0.025)));
+                        int y1graf = (int) Math.floor((this.getHeight() / 2) - (YP1 * (getHeight() * 0.025)));
+                        int y2graf = (int) Math.floor((this.getHeight() / 2) - (YP2 * (getHeight() * 0.025)));
+                        x1 = x2;
+                        x2 = x2 - .125;
                         g.drawLine(x1graf, y1graf, x2graf, y2graf);
                     }
                 }
 
-            }
-            else if(this.tipo==3) {
-                g.setColor(new Color(170, 140,7));
-                    if(this.tipoElipse==31) {//PARALELA X
-                        double a=Math.sqrt((BX*BX)+(CX*CX));
-                        double x1=a;
-                        double x2=a-.125;
-                        for(int z =0;z<((a*2)*8);z++) {
-                            double raiz1 = (1-((x1*x1)/(a*a)))*(BX*BX);
-                            double raiz2 = (1-((x2*x2)/(a*a)))*(BX*BX);
-                            YE1=Math.sqrt(raiz1);
-                            YE2=Math.sqrt(raiz2);        
-                            int x1graf = (int)Math.floor((this.getWidth()/2)+(x1*(getWidth()*0.025)));
-                            int x2graf = (int)Math.floor((this.getWidth()/2)+(x2*(getWidth()*0.025)));
-                            int y1graf = (int)Math.floor((this.getHeight()/2)-(YE1*(getHeight()*0.025)));
-                            int y2graf = (int)Math.floor((this.getHeight()/2)-(YE2*(getHeight()*0.025)));
+            } else if (this.tipo == 3) {
+                double b2=(AX*AX)-(CX*CX);
+                    double b = Math.sqrt(b2);
+                g.setColor(new Color(170, 140, 7));
+                if (this.tipoElipse == 31) {//PARALELA X
+                    
+                    
+                    for(int i = 0; i<2;i++) {
+                        double x1 = AX;
+                        double x2 = AX - .125;
+                        for (int z = 0; z < ((AX * 2) * 8); z++) {
+                            double raiz1 = (1 - ((x1 * x1) / (AX*AX))) * b2;
+                            double raiz2 = (1 - ((x2 * x2) / (AX*AX))) * b2;
+                            double YValue = Math.sqrt(raiz1);
+                            double YValue2 = Math.sqrt(raiz2);
+                            if(i ==0) {
+                                YE1 = YValue;
+                                YE2 = YValue2;
+                            } else {
+                                YE1 = -YValue;
+                                YE2 = -YValue2;
+                            }
+                            int x1graf = (int) Math.floor((this.getWidth() / 2) + (x1 * (getWidth() * 0.025)));
+                            int x2graf = (int) Math.floor((this.getWidth() / 2) + (x2 * (getWidth() * 0.025)));
+                            int y1graf, y2graf;
+
+                                y1graf = (int) Math.floor((this.getHeight() / 2) - (YE1 * (getHeight() * 0.025)));
+                                y2graf = (int) Math.floor((this.getHeight() / 2) - (YE2 * (getHeight() * 0.025)));
                             
-                            x1=x2;
-                            x2=x2-.125;
+                            x1 = x2;
+                            x2 = x2 - .125;
+                            g.drawLine(x1graf, y1graf, x2graf, y2graf);
+                        }
+                    }  
+                        
+                }
+                if (this.tipoElipse == 32) {//PARALELA Y
+                    for(int i = 0; i<2;i++) {
+                        double x1 = AX;
+                        double x2 = AX - .125;
+                        for (int z = 0; z < ((AX * 2) * 8); z++) {
+                            double raiz1 = (1 - ((x1 * x1) / (AX*AX))) * b2;
+                            double raiz2 = (1 - ((x2 * x2) / (AX*AX))) * b2;
+                            double YValue = Math.sqrt(raiz1);
+                            double YValue2 = Math.sqrt(raiz2);
+                            if(i ==0) {
+                                YE1 = YValue;
+                                YE2 = YValue2;
+                            } else {
+                                YE1 = -YValue;
+                                YE2 = -YValue2;
+                            }
+                            int y1graf = (int) Math.floor((this.getHeight() / 2) + (x1 * (getHeight() * 0.025)));
+                            int y2graf = (int) Math.floor((this.getHeight() / 2) + (x2 * (getHeight() * 0.025)));
+                            int x1graf, x2graf;
+
+                                x1graf = (int) Math.floor((this.getWidth() / 2) - (YE1 * (getWidth() * 0.025)));
+                                x2graf = (int) Math.floor((this.getWidth() / 2) - (YE2 * (getWidth() * 0.025)));
+                            
+                            x1 = x2;
+                            x2 = x2 - .125;
                             g.drawLine(x1graf, y1graf, x2graf, y2graf);
                         }
                     }
-                    if(this.tipoElipse==32) {//PARALELA Y
-                        double y1=Y2;
-                        double y2=Y2-.125;
-                        for(int z =0;z<(y1*2*4);z++) {
-                            
-                        
-                        
-                            y1=y2;
-                            y2=y2-.125;
-                            
-                        }
-                    }
-                    this.tipoElipse=0;
                 }
-            
+                this.tipoElipse = 0;
+            }
+
             //AQUI HAZ LAS OTRAS PINSHES TIPOS DE GRAFICAS
-                
-                
         }
-            
-            
-            
-            
-            
-        
-        
-        
-        
-        
-        
+
         g.setColor(Color.red);
         g.drawLine(0, this.getHeight() / 2, this.getWidth(), this.getHeight() / 2);
-        g.drawLine(this.getWidth() / 2, 0, this.getWidth()/2, this.getHeight());
-        
+        g.drawLine(this.getWidth() / 2, 0, this.getWidth() / 2, this.getHeight());
+
     }
 
     public void paintSQRFunc(Graphics g, double x2mult, double x1mult, double cons,
-            double x1, double x2){
-        
-        
-        
+            double x1, double x2) {
+
     }
 }
